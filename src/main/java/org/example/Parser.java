b115e640dd9bffe5fff0660b00f498ba153fe915
fopas <ash.03@yandex.ru>
2024-05-21 23:18:31 +0300

    done

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -1,35 +1,73 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.rabbitmq.client.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
-public class Parser {
-    public void parse() {
-        try {
-            Document doc = Jsoup.connect("https://habr.com/ru/feed/").get();
+public class Parser implements Runnable {
 
-            Elements posts = doc.select("article.tm-articles-list__item");
+    private final ConnectionFactory factory;
+    private static final Logger logger = LoggerFactory.getLogger(Parser.class);
 
-            for (Element post : posts) {
-                String author = post.select("a.tm-user-info__username").text();
-                String title = post.select("a.tm-title__link span").text().trim();
-                String articleText = post.select("div.article-formatted-body").text().trim();
-                String time = post.select("a.tm-article-datetime-published").text();
-                String link = "https://habr.com" + post.select("a.tm-title__link").attr("href");
+    public Parser(ConnectionFactory factory) {
+        this.factory = factory;
+    }
 
-                // Вывод информации о публикации
-                System.out.println("Author: " + author);
-                System.out.println("Title: " + title);
-                System.out.println("Article Text: " + articleText);
-                System.out.println("Time: " + time);
-                System.out.println("Link: " + link);
-                System.out.println();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    @Override
+    public void run() {
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.basicQos(1);
+
+            // Начало прослушивания очереди "link"
+//            channel.basicConsume("link", false, deliverCallback, consumerTag -> { });
+//
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String link = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                try {
+                    // Подтверждение получения сообщения
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+
+                    // Парсинг и публикация статьи
+                    Article article = parseArticle(link);
+                    if (article != null) {
+                        String json = new Gson().toJson(article);
+                        channel.basicPublish("", "info", null, json.getBytes());
+                        logger.info("Published article info: {}", json);
+                    }
+
+                    // Подтверждение получения сообщения
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                } catch (IOException e) {
+                    logger.error("Error occurred in parsing: ", e);
+                }
+            };
+        } catch (IOException | TimeoutException e) {
+            logger.error("Error occurred in parsing: ", e);
         }
+    }
+
+    private Article parseArticle(String url) {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            String title = doc.select("h1.tm-title.tm-title_h1 span").text().trim();
+            String author = doc.select("a.tm-user-info__username").text().trim();
+            String time = doc.select("span.tm-article-datetime-published time").attr("datetime");
+            StringBuilder text = new StringBuilder();
+            doc.select("div.article-formatted-body p").forEach(paragraph -> text.append(paragraph.text()).append("\n"));
+            return new Article(title, author, time, text.toString(), url);
+        } catch (IOException e) {
+            logger.error("Error parsing article from URL: " + url, e);
+        }
+        return null;
     }
 }
